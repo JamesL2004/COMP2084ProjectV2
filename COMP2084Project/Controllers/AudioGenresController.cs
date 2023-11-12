b950abcd059e4fe3e98a06469b0464742f40d710
JamesL2004 <James002>
2023-11-12 18:33:01 -0500

    Added authentication for all of the audio related conrollers.

--- COMP2084Project/Controllers/AudioGenresController.cs
+++ COMP2084Project/Controllers/AudioGenresController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using COMP2084Project.Data;
 using COMP2084Project.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace COMP2084Project.Controllers
 {
+    [Authorize]
     public class AudioGenresController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,6 +22,7 @@ namespace COMP2084Project.Controllers
         }
 
         // GET: AudioGenres
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
               return _context.AudioGenres != null ? 
@@ -28,6 +31,7 @@ namespace COMP2084Project.Controllers
         }
 
         // GET: AudioGenres/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.AudioGenres == null)
